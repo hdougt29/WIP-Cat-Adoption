402bbefa866555546a66a5f3d5465a07605f3184
hdougt29 <84687400+hdougt29@users.noreply.github.com>
2022-12-11 11:12:08 -0800

    Update Match.py

--- Match.py
+++ Match.py
@@ -1,20 +1,25 @@
 import tkinter as tk
 import sqlite3
 from tkinter  import messagebox
+from tkinter import OptionMenu
 
 # Create the root window
 root = tk.Tk()
 
 # Set the size of the root window to 600x400
-root.geometry("600x500")
+root.geometry("600x600")
 
 # Create a label with text
-title_label = tk.Label(root, text="Purrfect Match", font=("Helvetica", 24), bg="Dark orange")
+title_label = tk.Label(root, text="Purrfect Match", font=("Helvetica", 24))
 
 # Place the label in the root window
 title_label.pack()
 
-global name_entry, form, age_entry
+global name_entry, form, age_entry, activity_entry, activity_menu, rows
+
+####################################################################################################
+# Database Connection
+####################################################################################################
 
 # Create a database connection
 connection = sqlite3.connect("survey.db")
@@ -28,11 +33,111 @@ table_exists = cursor.fetchone()
 if not table_exists:
     cursor.execute("CREATE TABLE survey (name TEXT, age INTEGER)")
 
+# Try to add the activity column to the survey table
+try:
+    cursor.execute("ALTER TABLE survey ADD COLUMN activity TEXT")
+except sqlite3.OperationalError as error:
+    # Check if the error was caused by the activity column already existing
+    if "duplicate column name" in str(error):
+        # The activity column already exists, so we can ignore the error and continue
+        pass
+    else:
+        # The error was caused by something else, so we should raise the error again
+        raise error
+
+####################################################################################################
+# New Quiz
+####################################################################################################
+
+# Define a function that should be called when the button is clicked
+def on_new_quiz_click():
+    # This function creates and displays the quiz form
+
+    global name_entry, form, age_entry, activity_var, activity_menu
+
+    # Create the form window
+    form = tk.Toplevel(root)
+    
+    # Create a form field
+    age_label = tk.Label(form, text="What age animal are you looking for?")
+    age_entry = tk.Entry(form)
+    activity_label = tk.Label(form, text="What activity level are you looking for?")
+    activity_var = tk.StringVar(form)
+    activity_var.set("Lazy")  # Set the default value
+    activity_menu = tk.OptionMenu(form, activity_var, "Lazy", "Active", "Hyper")
+    
+    # Create a submit button
+    submit_button = tk.Button(form, text="Submit")
+    
+    # Place the form field and button in the form window
+    age_label.grid(row=0, column=0)
+    age_entry.grid(row=0, column=1)
+    activity_label.grid(row=1, column=0)
+    activity_menu.grid(row=1, column=1)
+    submit_button.grid(row=2, column=0, columnspan=2, pady=(10, 0))
+    
+    # Bind the submit button to a function that handles the form data
+    submit_button.bind("<Button-1>", on_new_quiz_submit)
+    
+    # Show the form
+    form.mainloop()
+
+def on_new_quiz_submit(event):
+    # This function handles the form data when the submit button is clicked
+
+    global name_entry, form, age_entry, activity_var, activity_menu, rows
+
+    # Get the data from the form field
+    age = age_entry.get()
+    activity = activity_var.get()
+    
+    # Query the database for records that match the specified age
+    cursor.execute("SELECT name, age, activity FROM survey WHERE age = ? AND activity = ?", (age, activity))
+
+    # Fetch the matching records
+    records = cursor.fetchall()
+
+    # If no records were found, display an error message
+    if not records:
+        messagebox.showwarning("No records found", "Sorry, there are no records that match your search criteria.")
+
+    # Otherwise, display the matching records in the Listbox widget
+    else:
+        # Clear the Listbox widget
+        listbox.delete(0, tk.END)
+
+        # Add the column names to the Listbox widget
+        listbox.insert(tk.END, "Name")
+        listbox.insert(tk.END, "Age")
+        listbox.insert(tk.END, "Activity")
+
+        # Add a separator line to the Listbox widget
+        listbox.insert(tk.END, "--------")
+
+        # Add the records to the Listbox widget
+        for record in records:
+            listbox.insert(tk.END, record[0])
+            listbox.insert(tk.END, str(record[1]))
+            listbox.insert(tk.END, record[2])
+
+    # Close the form window
+    form.destroy()
+
+# Create a button with text
+new_quiz_button = tk.Button(root, text="New Quiz", command=on_new_quiz_click, font=("Helvetica", 18) )
+
+# Place the button below the label
+new_quiz_button.pack()
+
+####################################################################################################
+# Add Animal
+####################################################################################################
+
 # Define a function that should be called when the button is clicked
 def on_button_click():
     # This function creates and displays the survey form
 
-    global name_entry, form, age_entry
+    global name_entry, form, age_entry, activity_menu, activity_variable
     
     # Create the form window
     form = tk.Toplevel(root)
@@ -42,6 +147,12 @@ def on_button_click():
     name_entry = tk.Entry(form)
     age_label = tk.Label(form, text="Age:")
     age_entry = tk.Entry(form)
+    activity_label = tk.Label(form, text="Activity:")
+    activity_variable = tk.StringVar(form)
+    activity_variable.set("Lazy") # default value
+    activity_menu = OptionMenu(form, activity_variable, "Lazy", "Active", "Hyper")
+    activity_label.grid(row=2, column=0, pady=(10, 0))
+    activity_menu.grid(row=2, column=1)
     
     # Create a submit button
     submit_button = tk.Button(form, text="Submit")
@@ -51,7 +162,9 @@ def on_button_click():
     name_entry.grid(row=0, column=1)
     age_label.grid(row=1, column=0, pady=(10, 0))
     age_entry.grid(row=1, column=1)
-    submit_button.grid(row=2, column=0, columnspan=2, pady=(10, 0))
+    activity_label.grid(row=2, column=0, pady=(10,0))
+    activity_menu.grid(row=2, column=1)
+    submit_button.grid(row=3, column=0, columnspan=2, pady=(10, 0))
     
     # Bind the submit button to a function that handles the form data
     submit_button.bind("<Button-1>", on_submit)
@@ -62,14 +175,15 @@ def on_button_click():
 def on_submit(event):
     # This function handles the form data when the submit button is clicked
 
-    global name_entry, form, age_entry
+    global name_entry, form, age_entry, activity_variable
     
     # Get the data from the form fields
     name = name_entry.get()
     age = age_entry.get()
+    activity = activity_variable.get()
     
     # Add the data to the database
-    cursor.execute("INSERT INTO survey (name, age) VALUES (?, ?)", (name, age))
+    cursor.execute("INSERT INTO survey (name, age, activity) VALUES (?, ?, ?)", (name, age, activity))
     connection.commit()
     
     # Close the form window
@@ -84,25 +198,29 @@ add_button = tk.Button(root, text="Add Animal", command=on_button_click, font=("
 # Place the button below the label
 add_button.pack()
 
+####################################################################################################
+# Survey Table
+####################################################################################################
+
 # Create a Listbox widget in the root window
 listbox = tk.Listbox(root, font=("Helvetica", 18))
 
 # Query the database and update the Listbox widget
 def update_listbox():
 
-    global name_entry, form, age_entry
+    global name_entry, form, age_entry, rows
 
     # Clear the Listbox widget
     listbox.delete(0, tk.END)
 
     # Add the column names to the Listbox widget
-    listbox.insert(tk.END, ("Name", "Age"))
+    listbox.insert(tk.END, ("Name", "Age", "Activity"))
 
     # Add padding to the column names
     listbox.insert(tk.END, ("", ""))
 
     # Query the database
-    cursor.execute("SELECT name, age FROM survey")
+    cursor.execute("SELECT name, age, activity FROM survey")
     rows = cursor.fetchall()
 
     # Loop through the rows of data and add each row to the Listbox widget
@@ -118,10 +236,31 @@ update_listbox()
 # Place the Listbox widget in the root window
 listbox.pack()
 
+####################################################################################################
+# Refresh
+####################################################################################################
+
+# Define a function that should be called when the refresh button is clicked
+def on_refresh_click():
+    # This function updates the Listbox widget to show all records in the survey table
+
+    # Query the database and update the Listbox widget
+    update_listbox()
+
+# Create a refresh button
+refresh_button = tk.Button(root, text="Refresh", command=on_refresh_click, font=("Helvetica", 18))
+
+# Place the refresh button below the new quiz button
+refresh_button.pack()
+
+####################################################################################################
+# Edit Animal
+####################################################################################################
+
 def on_edit_click(event):
     # This function handles the editing of records
 
-    global name_entry, form, age_entry
+    global name_entry, form, age_entry, activity_menu, activity_label, activity_var
 
     # Get the selected item in the Listbox widget
     index = listbox.curselection()
@@ -130,6 +269,7 @@ def on_edit_click(event):
     # Get the values for the selected record
     name = item[0]
     age = item[1]
+    activity = item[2]
 
     # Create the form window
     form = tk.Toplevel(root)
@@ -141,16 +281,32 @@ def on_edit_click(event):
     age_label = tk.Label(form, text="Age:")
     age_entry = tk.Entry(form)
     age_entry.insert(0, age)
+    activity_label = tk.Label(form, text="Activity:")
+    
 
     # Create a submit button
     submit_button = tk.Button(form, text="Submit")
+
+    # Create a list of available activities
+    activities = ["Lazy", "Active", "Hyper"]
+
+    # Create a Tkinter variable to hold the selected activity
+    activity_var = tk.StringVar()
+
+    # Set the value of the Tkinter variable to the current activity
+    activity_var.set(activity)
+
+    # Create an OptionMenu widget for the activity
+    activity_menu = tk.OptionMenu(form, activity_var, *activities)
 
     # Place the form fields and button in the form window
     name_label.grid(row=0, column=0)
     name_entry.grid(row=0, column=1)
     age_label.grid(row=1, column=0, pady=(10, 0))
     age_entry.grid(row=1, column=1)
-    submit_button.grid(row=2, column=0, columnspan=2, pady=(10, 0))
+    activity_label.grid(row=2, column=0, pady=(10, 0))
+    activity_menu.grid(row=2, column=1)
+    submit_button.grid(row=3, column=0, columnspan=2, pady=(10, 0))
 
     # Bind the submit button to a function that handles the form data
     submit_button.bind("<Button-1>", on_edit_submit)
@@ -161,15 +317,24 @@ def on_edit_click(event):
 def on_edit_submit(event):
     # This function handles the form data when the submit button is clicked
 
-    global name_entry, form, age_entry
+    global name_entry, form, age_entry, activity_menu, activity_var
 
     # Get the data from the form fields
-    name = name_entry.get()
-    age = age_entry.get()
+    new_name = name_entry.get()
+    new_age = age_entry.get()
+    new_activity = activity_var.get()
+
+    # Get the selected item in the Listbox widget
+    index = listbox.curselection()
+    item = listbox.get(index)
+
+    # Get the values for the selected record
+    old_name = item[0]
+    old_age = item[1]
+    old_activity = item[2]
 
     # Update the database with the new values
-    cursor.execute("UPDATE survey SET name = ?, age = ? WHERE name = ?", (name, age, name))
-    connection.commit()
+    cursor.execute("UPDATE survey SET name = ?, age = ?, activity = ? WHERE name = ? AND age = ? AND activity = ?", (new_name, new_age, new_activity, old_name, old_age, old_activity))
 
     # Close the form window
     form.destroy()
@@ -186,6 +351,10 @@ edit_button.bind("<Button-1>", on_edit_click)
 # Place the edit button below the add button
 edit_button.pack()
 
+####################################################################################################
+# Delete Animal
+####################################################################################################
+
 # Define a function that should be called when the "Delete" button is clicked
 def on_delete_click():
     # This function handles the deletion of records
@@ -195,7 +364,7 @@ def on_delete_click():
     item = listbox.get(index)
 
     # Get the values for the selected record
-    name, age = item
+    name, age, activity = item
 
     # Ask the user for confirmation before deleting the record
     result = messagebox.askyesno("Confirm", "Are you sure you want to delete this record?")
@@ -216,4 +385,5 @@ delete_button.pack()
 
 # Run the main loop
 root.mainloop()
+
 
